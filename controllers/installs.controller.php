9767efeab492df063735d8dd09cbb4d922c5c34e
Adwi Arifin <adwiarifin@gmail.com>
2018-02-07 23:58:44 +0700

    change from list to table with checkbox

--- controllers/installs.controller.php
+++ controllers/installs.controller.php
@@ -8,6 +8,13 @@ class InstallsController extends Controller {
     }
 
 	public function admin_index() {
+		if ( $_POST ) {
+			echo "<pre>";
+			print_r($_POST);
+			echo "</pre>";
+			exit();
+		}
+
 		$this->data = $this->model->getTableList();
 	}
 }
